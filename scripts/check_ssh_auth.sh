497983274ec14c55a2aa6770f3fa291041d0b3cc
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2025-02-02 21:40:25 -0800

    add some colour output the scripts

--- scripts/check_ssh_auth.sh
+++ scripts/check_ssh_auth.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+source ./colours.sh
+
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <hostname-or-ip>"
+  echo
+  echo -e "${YELLOW}Usage: $0 <hostname-or-ip>${NC}"
+  echo
   exit 1
 fi
 
 # Extract the server from the first argument
 server="$1"
 
+echo
 # Execute the SSH command to check supported authentication methods
 ssh -o PreferredAuthentications=none -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "nonexistent-username@$server"
+echo
--- scripts/fzf_upgrade.sh
+++ scripts/fzf_upgrade.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Set the desired version of fzf
-#FZF_VERSION="0.27.2" # Change this to the desired version
+source ./colours.sh
 
 # display current fzf version
-echo "Current fzf version: "
+echo -e "${CYAN}Current fzf version:${NC} "
 fzf --version
 echo
 
@@ -13,21 +12,21 @@ INSTALL_DIR="$HOME/.fzf"
 
 # Remove existing installation if it exists
 if [ -d "$INSTALL_DIR" ]; then
-  echo "Removing existing fzf installation..."
+  echo -e "${YELLOW}Removing existing fzf installation...${NC}"
   rm -rf "$INSTALL_DIR"
 fi
 
 # Clone the fzf repository from GitHub
-echo "Cloning fzf from GitHub..."
+echo -e "${GREEN}Cloning fzf from GitHub...${NC}"
 git clone --depth 1 https://github.com/junegunn/fzf.git "$INSTALL_DIR"
 
 # Run the installation script
-echo "Running fzf installation script..."
+echo -e "${GREEN}Running fzf installation script...${NC}"
 "$INSTALL_DIR/install" --key-bindings --completion --no-update-rc
 
-echo "fzf upgraded successfully to version $FZF_VERSION"
+echo -e "${MAGENTA}fzf upgraded successfully${NC}"
 echo
 
 # display current fzf version
-echo "Upgraded fzf version: "
+echo -e "${CYAN}Upgraded fzf version:${NC} "
 fzf --version
