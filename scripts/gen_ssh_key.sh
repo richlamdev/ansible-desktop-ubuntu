27141491699a0ff79a87355bed703462885ad785
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2025-02-24 00:21:31 -0800

    update gen ssh script

--- scripts/gen_ssh_key.sh
+++ scripts/gen_ssh_key.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
+# Load color definitions for output
 source ./colours.sh
 
-# Set HOSTNAME to the hostname without domain
+# Introductory message
+echo -e "${CYAN}This script will generate an SSH key pair for your localhost.${NC}"
+
+# Define variables
 USER=$(whoami)
 HOSTNAME=$(hostname -s)
 KEY_TYPE="ed25519"
 DATE=$(date +"%Y%m%d")
-
-# Generate the SSH key pair with the specified filename and passphrase prompt
-echo
 KEY_NAME="${HOSTNAME}_${DATE}_${KEY_TYPE}_key"
-ssh-keygen -t "$KEY_TYPE" -f "$HOME/.ssh/$KEY_NAME" -N "$(
-  read -s -p 'Enter passphrase for the key: '
-  echo $REPLY
-)"
+KEY_PATH="$HOME/.ssh/$KEY_NAME"
+AUTH_KEYS="$HOME/.ssh/authorized_keys"
 
-# Check if the .ssh directory exists, and create it if not
-if [ ! -d "$HOME/.ssh" ]; then
-  mkdir -p "$HOME/.ssh"
-fi
+# Ensure the .ssh directory exists
+mkdir -p "$HOME/.ssh"
 
-# Copy the public key to authorized_keys, overwriting it
-cp "$HOME/.ssh/$KEY_NAME.pub" "$HOME/.ssh/authorized_keys"
-
-# Secure file permissions
-chmod 700 "$HOME/.ssh"
-chmod 600 "$HOME/.ssh/$KEY_NAME"
-chmod 644 "$HOME/.ssh/authorized_keys"
-
-# Output the location of the private and public keys
+# Prompt for passphrase and generate SSH key pair
 echo
-echo -e "${CYAN}SSH key pair generated and public key copied to authorized_keys (overwriting existing keys).${NC}"
-echo -e "${GREEN}Private key: $HOME/.ssh/$KEY_NAME${NC}"
-echo -e "${GREEN}Public key: $HOME/.ssh/$KEY_NAME.pub${NC}"
+read -s -p 'Enter passphrase for the key (leave blank for no passphrase): ' PASSPHRASE
+echo
+
+# Generate an SSH key pair
+ssh-keygen -t "$KEY_TYPE" -f "$KEY_PATH" -N "$PASSPHRASE"
+
+# Copy the public key to authorized_keys, overwriting existing keys
+cat "$KEY_PATH.pub" >"$AUTH_KEYS"
+
+# Set secure permissions for .ssh directory and key files
+chmod 700 "$HOME/.ssh"
+chmod 600 "$KEY_PATH"
+chmod 644 "$AUTH_KEYS"
+
+# Output the location of the keys
+echo
+echo -e "${CYAN}SSH key pair generated and public key copied to ${AUTH_KEYS} (overwriting existing keys).${NC}"
+echo -e "${GREEN}Private key: $KEY_PATH${NC}"
+echo -e "${GREEN}Public key: $KEY_PATH.pub${NC}"
 echo
 
 # Test the key
 echo -e "${YELLOW}Testing the key with the passphrase...${NC}"
-echo -e "${RED}** DO NOT FORGET TO EXIT THIS SESSION! ***${NC}"
 echo
-if ssh -i "$HOME/.ssh/$KEY_NAME" $USER@localhost; then
+
+if ssh -i "$KEY_PATH" "$USER@localhost" true; then
   echo
-  echo -e "${GREEN}Key test successful. You can use the key for SSH authentication.{NC}"
-  echo
+  echo -e "${GREEN}Key test successful. You can use the key for SSH authentication.${NC}"
 else
   echo
-  echo -e "${RED}Key test failed. Please double-check the passphrase and try again.{NC}"
-  echo
+  echo -e "${RED}Key test failed. Please double-check the passphrase and try again.${NC}"
 fi
 
 exit 0
