f7110f1ef99a76ada8a946a17b91b5c306909a60
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2025-02-24 00:39:44 -0800

    update desktop setup script

--- scripts/desktop_setup.sh
+++ scripts/desktop_setup.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 # Import/Export GNOME desktop settings
 
+# Source the colors script
+source ./colours.sh
+
 SAVED_FILE="dconf-settings.ini"
 
 if [ "$1" == "save" ]; then
   # Save all GNOME settings to a file
   dconf dump / >"$SAVED_FILE"
-  echo "Settings saved to $SAVED_FILE"
+  echo -e "${GREEN}Settings saved to $SAVED_FILE${NC}"
 
 elif [ "$1" == "load" ]; then
   # Load all GNOME settings from a file
   if [ -f "$SAVED_FILE" ]; then
     dconf load / <"$SAVED_FILE"
-    echo "Settings loaded from $SAVED_FILE"
+    echo -e "${GREEN}Settings loaded from $SAVED_FILE${NC}"
   else
-    echo "No saved settings file found! Expected file: $SAVED_FILE"
+    echo -e "${RED}No saved settings file found! Expected file: $SAVED_FILE${NC}"
   fi
 
 else
-  echo "Usage: $0 {save|load}"
-  echo "  save  - Save all GNOME settings to $SAVED_FILE"
-  echo "  load  - Load all GNOME settings from $SAVED_FILE"
+  echo -e "${YELLOW}Usage: $0 {save|load}${NC}"
+  echo -e "${YELLOW}  save  - Save all GNOME settings to $SAVED_FILE${NC}"
+  echo -e "${YELLOW}  load  - Load all GNOME settings from $SAVED_FILE${NC}"
 fi
-
-# import Gnome desktop settings
-
-# to save settings
-#dconf dump / > dconf-settings.ini
-
-#dconf load / < dconf-settings.ini
