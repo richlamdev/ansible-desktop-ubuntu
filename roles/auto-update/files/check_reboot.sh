45eab6d9210a3e0c61e237dda05bba7120cea5a5
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2025-10-18 20:55:58 -0700

    tag roles with same name for easier execution

--- roles/auto-update/files/check_reboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOG_FILE="/var/log/reboot_check.log"
-
-if [ -f /var/run/reboot-required ]; then
-  echo "Reboot required on $(date)" >>"$LOG_FILE"
-  # Uncomment the next line to automatically reboot
-  /sbin/reboot
-fi
