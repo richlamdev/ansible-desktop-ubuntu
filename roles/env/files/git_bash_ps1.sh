9edc5b619f2ab3e179cbd7e5e11e54e78bb1775e
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2026-09-20 22:49:42 -0700

    update ps1

--- roles/env/files/git_bash_ps1.sh
+++ roles/env/files/git_bash_ps1.sh
@@ -1,118 +1,31 @@
-#  Customize BASH PS1 prompt to show current GIT repository and branch.
-#  by Mike Stewart - http://MediaDoneRight.com
+Color_Off='\[\033[0m\]'
+Blue027='\[\033[01;38;5;027m\]'
+IPurple='\[\033[0;95m\]'
+IGreen='\[\033[0;92m\]'
+BWhite='\[\033[1;37m\]'
+BGreen='\[\033[1;32m\]'
+BIRed='\[\033[1;91m\]'
+Yellow226='\[\033[01;38;5;226m\]'
+IYellow='\[\033[0;93m\]'
 
-#  SETUP CONSTANTS
-#  Bunch-o-predefined colors.  Makes reading code easier than escape sequences.
-#  I don't remember where I found this.  o_O
+__build_ps1() {
+  local git_part=""
 
-# Reset
-Color_Off="\[\033[0m\]" # Text Reset
+  if git rev-parse --is-inside-work-tree &>/dev/null; then
+    local head_rev branch
+    head_rev=$(git log --pretty=%h -n 1 2>/dev/null || echo "no-commits")
+    branch=$(git symbolic-ref --short HEAD 2>/dev/null || echo "detached")
 
-# Regular Colors
-Black="\[\033[0;30m\]"  # Black
-Red="\[\033[0;31m\]"    # Red
-Green="\[\033[0;32m\]"  # Green
-Yellow="\[\033[0;33m\]" # Yellow
-Blue="\[\033[0;34m\]"   # Blue
-Purple="\[\033[0;35m\]" # Purple
-Cyan="\[\033[0;36m\]"   # Cyan
-White="\[\033[0;37m\]"  # White
+    if git diff --quiet 2>/dev/null && git diff --cached --quiet 2>/dev/null; then
+      git_part="${BWhite}|${head_rev}|${BGreen}(${branch})${BWhite}|${IYellow}\w${Color_Off}"
+    else
+      git_part="${BWhite}|${head_rev}|${BIRed}{${branch}}${BWhite}|${IYellow}\w${Color_Off}"
+    fi
+  else
+    git_part="${Yellow226}\w${Color_Off}"
+  fi
 
-Yellow226="\[\033[01;38;5;226m\]"
-LightPink207="\[\033[01;38;5;207m\]"
-Blue027="\[\033[01;38;5;027m\]"
+  PS1="${Blue027}\u${IPurple}@${IGreen}\h${Color_Off}${git_part} \$ "
+}
 
-# Bold
-BBlack="\[\033[1;30m\]"  # Black
-BRed="\[\033[1;31m\]"    # Red
-BGreen="\[\033[1;32m\]"  # Green
-BYellow="\[\033[1;33m\]" # Yellow
-BBlue="\[\033[1;34m\]"   # Blue
-BPurple="\[\033[1;35m\]" # Purple
-BCyan="\[\033[1;36m\]"   # Cyan
-BWhite="\[\033[1;37m\]"  # White
-
-# Underline
-UBlack="\[\033[4;30m\]"  # Black
-URed="\[\033[4;31m\]"    # Red
-UGreen="\[\033[4;32m\]"  # Green
-UYellow="\[\033[4;33m\]" # Yellow
-UBlue="\[\033[4;34m\]"   # Blue
-UPurple="\[\033[4;35m\]" # Purple
-UCyan="\[\033[4;36m\]"   # Cyan
-UWhite="\[\033[4;37m\]"  # White
-
-# Background
-On_Black="\[\033[40m\]"  # Black
-On_Red="\[\033[41m\]"    # Red
-On_Green="\[\033[42m\]"  # Green
-On_Yellow="\[\033[43m\]" # Yellow
-On_Blue="\[\033[44m\]"   # Blue
-On_Purple="\[\033[45m\]" # Purple
-On_Cyan="\[\033[46m\]"   # Cyan
-On_White="\[\033[47m\]"  # White
-
-# High Intensty
-IBlack="\[\033[0;90m\]"  # Black
-IRed="\[\033[0;91m\]"    # Red
-IGreen="\[\033[0;92m\]"  # Green
-IYellow="\[\033[0;93m\]" # Yellow
-IBlue="\[\033[0;94m\]"   # Blue
-IPurple="\[\033[0;95m\]" # Purple
-ICyan="\[\033[0;96m\]"   # Cyan
-IWhite="\[\033[0;97m\]"  # White
-
-# Bold High Intensty
-BIBlack="\[\033[1;90m\]"  # Black
-BIRed="\[\033[1;91m\]"    # Red
-BIGreen="\[\033[1;92m\]"  # Green
-BIYellow="\[\033[1;93m\]" # Yellow
-BIBlue="\[\033[1;94m\]"   # Blue
-BIPurple="\[\033[1;95m\]" # Purple
-BICyan="\[\033[1;96m\]"   # Cyan
-BIWhite="\[\033[1;97m\]"  # White
-
-# High Intensty backgrounds
-On_IBlack="\[\033[0;100m\]"  # Black
-On_IRed="\[\033[0;101m\]"    # Red
-On_IGreen="\[\033[0;102m\]"  # Green
-On_IYellow="\[\033[0;103m\]" # Yellow
-On_IBlue="\[\033[0;104m\]"   # Blue
-On_IPurple="\[\033[10;95m\]" # Purple
-On_ICyan="\[\033[0;106m\]"   # Cyan
-On_IWhite="\[\033[0;107m\]"  # White
-
-# Various variables you might want for your PS1 prompt instead
-Time12h="\T"
-Time12a="\@"
-ShortHost="\h"
-Username="\u"
-PathShort="\W"
-PathFull="\w"
-NewLine="\n"
-Jobs="\j"
-
-# This PS1 snippet was adopted from code for MAC/BSD I saw from: http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
-# I tweaked it to work on UBUNTU 11.04 & 11.10 plus made it mo' better
-
-MyPS1=$Blue027$Username$IPurple@$IGreen$ShortHost$Color_Off'$(git branch &>/dev/null;\
-if [ $? -eq 0 ]; then \
-  echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
-  HEADREV=$(git log --pretty=%h -n 1 2>/dev/null || echo "no-commits")
-
-  echo "'$BWhite'|$HEADREV" | tr -d "\n"; \
-  if [ "$?" -eq "0" ]; then \
-    # @4 - Clean repository - nothing to commit
-    echo "|'$BGreen'"$(__git_ps1 "(%s)"); \
-  else \
-    # @5 - Changes to working tree
-    echo "|'$BIRed'"$(__git_ps1 "{%s}"); \
-  fi)'$BWhite'|'$IYellow$PathFull$Color_Off'"; \
-else \
-  # @2 - Prompt when not in GIT repo
-  echo "|'$Yellow226$PathFull$Color_Off'"; \
-fi)'
-
-export PS1="$IPurple[$Color_Off $MyPS1 \$ $IPurple]$Color_Off "
-# without bracket
-export PS1="$MyPS1 \$ "
+PROMPT_COMMAND=__build_ps1
